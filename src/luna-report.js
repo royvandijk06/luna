1d5fc9b23fb7fedf0f6519e0a4559d50d933a8d5
R.E.L. van Dijk <r.e.l.v.dijk@student.tue.nl>
2022-09-12 00:49:33 +0200

    V1.9.2

--- src/luna-report.js
+++ src/luna-report.js
@@ -5,27 +5,27 @@ const ejs = require("ejs");
 
 async function renderOutput(outputPath, data) {
     let paths = {
-        "cy":            resolve(".", "assets", "js", "cytoscape.min.js"),
-        "cyCollapse":    resolve(".", "assets", "js", "cytoscape-expand-collapse.js"),
-        "elk":           resolve(".", "assets", "js", "elk.bundled.js"),
-        "cyElk":         resolve(".", "assets", "js", "cytoscape-elk.js"),
-        "cola":          resolve(".", "assets", "js", "cola.min.js"),
-        "cyCola":        resolve(".", "assets", "js", "cytoscape-cola.js"),
-        "shim":          resolve(".", "assets", "js", "shim.min.js"),
-        "layoutBase":    resolve(".", "assets", "js", "layout-base.js"),
-        "coseBase":      resolve(".", "assets", "js", "cose-base.js"),
-        "cyCoseBilkent": resolve(".", "assets", "js", "cytoscape-cose-bilkent.js"),
-        "cySetup":       resolve(".", "assets", "js", "cytoscape-setup.js"),
-        "stylesheet":    resolve(".", "assets", "css", "stylesheet.css"),
-        "fontawesome":   resolve(".", "assets", "css", "fontawesome.min.css"),
-        "regularFont":   resolve(".", "assets", "css", "regular.min.css"),
-        "solidFont":     resolve(".", "assets", "css", "solid.min.css"),
-        "template":      resolve(".", "assets", "html", "template.ejs"),
+        "cy":            resolve(__dirname, "..", "assets", "js", "cytoscape.min.js"),
+        "cyCollapse":    resolve(__dirname, "..", "assets", "js", "cytoscape-expand-collapse.js"),
+        "elk":           resolve(__dirname, "..", "assets", "js", "elk.bundled.js"),
+        "cyElk":         resolve(__dirname, "..", "assets", "js", "cytoscape-elk.js"),
+        "cola":          resolve(__dirname, "..", "assets", "js", "cola.min.js"),
+        "cyCola":        resolve(__dirname, "..", "assets", "js", "cytoscape-cola.js"),
+        "shim":          resolve(__dirname, "..", "assets", "js", "shim.min.js"),
+        "layoutBase":    resolve(__dirname, "..", "assets", "js", "layout-base.js"),
+        "coseBase":      resolve(__dirname, "..", "assets", "js", "cose-base.js"),
+        "cyCoseBilkent": resolve(__dirname, "..", "assets", "js", "cytoscape-cose-bilkent.js"),
+        "cySetup":       resolve(__dirname, "..", "assets", "js", "cytoscape-setup.js"),
+        "stylesheet":    resolve(__dirname, "..", "assets", "css", "stylesheet.css"),
+        "fontawesome":   resolve(__dirname, "..", "assets", "css", "fontawesome.min.css"),
+        "regularFont":   resolve(__dirname, "..", "assets", "css", "regular.min.css"),
+        "solidFont":     resolve(__dirname, "..", "assets", "css", "solid.min.css"),
+        "template":      resolve(__dirname, "..", "assets", "html", "template.ejs"),
         "output":        resolve(outputPath),
     };
     let title = `${data.name || paths.output}${data.version ? `@${data.version}` : ""}`;
     let html = await ejs.renderFile(paths.template, { "debug": global.debug, title, paths, "data": data.data, "version": pkg.version }, { "rmWhitespace": true });
-    let result = resolve(outputPath, "./luna.html");
+    let result = resolve(outputPath, "luna.html");
     return writeFile(result, html).then(() => result);
 }
 
